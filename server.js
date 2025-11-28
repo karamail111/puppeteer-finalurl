063cd404cc76511da1921feb9793ecac4423ac84
karamail111 <76203345+karamail111@users.noreply.github.com>
2025-11-28 14:54:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("/clickgame", async (req, res) => {
 // 🎲 สุ่มเลือก 1 รูป
     const randomImage = images[Math.floor(Math.random() * images.length)];
     
-    const selector = "img[src*='/image/gameIcon/PG/PG-SLOT-149.png']";
+    const selector = "img[src*='/image/gameIcon/PG/PG-SLOT-058.png']";
 
     try {
       await page.waitForSelector(selector, { timeout: 8000 });
